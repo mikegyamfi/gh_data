dd5e9466d2d3d4d99d3b37d1e5eaa411f310d806
Michael Gyamfi <88879035+mikegyamfi@users.noreply.github.com>
2024-10-09 13:06:54 +0000

    Update settings.py

--- intel/settings.py
+++ intel/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'gh-data-lvhew.ondigitalocean.app',
     'www.ghdata.store',
-    'gh-data-store-341a796c74b9.herokuapp.com'
+    'gh-data-store-341a796c74b9.herokuapp.com',
+    'www.ghstorehub.com'
 ]
 
 # Application definition
