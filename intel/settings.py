71f90ac63551e3218ba0a0ca9a1c94eb3c8f9f2a
mikegaymfi <codeage20@gmail.com>
2025-06-23 09:48:45 +0000

    make paystack popup

--- intel/settings.py
+++ intel/settings.py
@@ -97,27 +97,27 @@ WSGI_APPLICATION = 'intel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': config("DATABASE_HOST"),
-#         'PORT': config("DATABASE_PORT"),
-#         'NAME': config('DATABASE_NAME'),
-#         'USER': config("DATABASE_USERNAME"),
-#         'PASSWORD': config("DATABASE_PASSWORD"),
-#         'OPTIONS': {
-#             'sslmode': 'require'
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
+        'NAME': config('DATABASE_NAME'),
+        'USER': config("DATABASE_USERNAME"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'OPTIONS': {
+            'sslmode': 'require'
+        }
+    }
+}
 
 # import os
 # import psycopg2
